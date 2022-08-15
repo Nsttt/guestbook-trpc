cafa4f7209ab5b3ef7c6839a634bee6626733ec8
Nestor <elnestinatorHD@gmail.com>
2022-08-15 16:05:30 +0200

    Add mutation to create new messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
+import { useState } from "react";
 import { trpc } from "../utils/trpc";
 
 const Messages = () => {
@@ -27,6 +28,9 @@ const Messages = () => {
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
 
+  const [message, setMessage] = useState("");
+  const postMessage = trpc.useMutation("guestbookpostNewMessage", {});
+
   if (status === "loading") {
     return <main className="flex flex-col items-center pt-4">Loading...</main>;
   }
@@ -45,17 +49,46 @@ const Home: NextPage = () => {
           {session ? (
             <div>
               <p>hi {session.user?.name}</p>
-
               <button onClick={() => signOut()}>Logout</button>
+              <div className="pt-6">
+                <form
+                  className="flex gap-2"
+                  onSubmit={(event) => {
+                    event.preventDefault();
+
+                    postMessage.mutate({
+                      name: session.user?.name as string,
+                      message,
+                    });
+
+                    setMessage("");
+                  }}
+                >
+                  <input
+                    type="text"
+                    value={message}
+                    placeholder="Your message"
+                    maxLength={100}
+                    onChange={(event) => setMessage(event.target.value)}
+                    className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 fecous:outline-none"
+                  />
+                  <button
+                    type="submit"
+                    className="p-2 rounded-md border-2 border-zinc-800 focus:outline-none"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+              <div className="pt-10">
+                <Messages />
+              </div>
             </div>
           ) : (
             <div>
               <button onClick={() => signIn("discord")}>
                 Login with Discord
               </button>
-
-              <div className="pt-10" />
-              <Messages />
             </div>
           )}
         </div>
